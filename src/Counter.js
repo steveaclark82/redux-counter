dc9d603a5905655411632a08256896ec49daa2ac
Steve Clark <steve.a.clark48@gmail.com>
2022-02-16 11:21:26 -0600

    Added buttons to Counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,22 +1,33 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
 class Counter extends React.Component {
   increment = () => {
     this.props.dispatch({
-      type: 'Increment'
+      type: "Increment",
+    });
+  };
+
+  incrementByFive = () => {
+    this.props.dispatch({
+      type: "IncrementByFive",
     });
   };
 
   decrement = () => {
     this.props.dispatch({
-      type: 'Decrement'
+      type: "Decrement",
     });
   };
 
+  decrementByTen = () => {
+    this.props.dispatch({
+      type: "DecrementByTen",
+    });
+  };
   reset = () => {
     this.props.dispatch({
-      type: 'Reset'
+      type: "Reset",
     });
   };
 
@@ -25,9 +36,15 @@ class Counter extends React.Component {
       <div>
         <h2>Counter</h2>
         <div>
-          <button onClick={this.decrement}>-</button>
           <span>{this.props.count}</span>
-          <button onClick={this.increment}>+</button>
+          <br />
+          <button onClick={this.increment}>Increase By 1</button>
+          <br />
+          <button onClick={this.incrementByFive}>Increase By 5</button>
+          <br />
+          <button onClick={this.decrement}>Decrease By 1</button>
+          <br />
+          <button onClick={this.decrementByTen}>Decrease By 10</button>
           <br />
           <button onClick={this.reset}>Reset</button>
         </div>
@@ -38,8 +55,10 @@ class Counter extends React.Component {
 
 function mapStateToProps(state) {
   return {
-    count: state.count
+    count: state.count,
   };
 }
+
+export default connect(mapStateToProps)(Counter);
 
 export default connect(mapStateToProps)(Counter);
